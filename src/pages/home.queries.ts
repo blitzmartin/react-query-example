ad436b9a0373e9cf5246ae4760c966b2ae823c47
blitzmartin <83599934+blitzmartin@users.noreply.github.com>
2023-10-17 16:29:44 +0000

    add example for mutation

--- src/pages/home.queries.ts
+++ src/pages/home.queries.ts
@@ -2,18 +2,55 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
 const getData = async (
-  ): Promise<DataResponse> => {
+): Promise<DataResponse> => {
     const response = await axios.get('/add/endpoint')
     return response.data // response or response.data?
-  }
-  
-  export const useData = () =>
+}
+
+export const useData = () =>
     useQuery({
-      queryKey: ['dataKey'],
-      queryFn: () => getData(),
+        queryKey: ['dataKey'],
+        queryFn: () => getData(),
     })
 
-    export type DataResponse = {
-        id: string
-        other: string
-    }
+export type DataResponse = {
+    id: string
+    other: string
+}
+
+
+/*
+const antivirusActivate = async (body: AntivirusActivateRequest) => {
+    const response = await axiosInstance.post('/api/v1/antivirus/activate', body)
+    return response.data
+}
+
+export const useAntivirusActivateMutation = () => {
+    return useMutation({
+        mutationFn: antivirusActivate,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['antivirusStatus'])
+        },
+    })
+}
+
+  const antivirusActivateMutation = useAntivirusActivateMutation()
+
+  const onToggleAntivirusSubmit = async (values: AntivirusActivationValues) => {
+    antivirusActivateMutation.mutate(
+      {
+        tenantId: tenant.id,
+        apiKey: values.avApiKey,
+      },
+      {
+        onSuccess: () => {
+          toast({
+            description: t('antivirus-activated'),
+            variant: 'success',
+          })
+        },
+      }
+    )
+  }
+
+*/
