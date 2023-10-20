c9774a22e744db7db89ca87a44391d47d00a768c
blitzmartin <lizmartinmusic@gmail.com>
2023-10-20 19:30:11 +0200

    move sidebar in applayout and change avatar

--- src/config/AppLayout.tsx
+++ src/config/AppLayout.tsx
@@ -1,4 +1,11 @@
-import { Sidebar, Toaster } from '@/shared'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Separator,
+  Toaster
+} from '@/shared'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import { Outlet } from 'react-router'
@@ -13,5 +20,43 @@ export const AppLayout = () => {
       </div>
       <Toaster />
     </QueryClientProvider>
+  )
+}
+
+const Sidebar = () => {
+  return (
+    <div className="flex h-screen min-w-[240px] flex-col items-center justify-start gap-16 bg-primary px-6  py-8">
+      <Avatar className="h-32 w-32">
+        <AvatarImage src="https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?auto=format&fit=crop&q=80&w=1160&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <AvatarFallback>MR</AvatarFallback>
+      </Avatar>
+      <div className="flex h-full w-full flex-col items-start justify-start gap-6">
+        <div className="flex w-full flex-col items-start justify-start gap-1">
+          <h2 className="font-display text-xl text-background">USERNAME</h2>
+          <Separator className="h-0.5 bg-background" />
+          <p className="text-sm">blitzmartin</p>
+        </div>
+        <div className="flex w-full flex-col items-start justify-start gap-1">
+          <h2 className="font-display text-xl text-background">
+            FIRST & LAST NAME
+          </h2>
+          <Separator className="h-0.5 bg-background" />
+          <p className="text-sm">annalisa de martino</p>
+        </div>
+        <div className="flex w-full flex-col items-start justify-start gap-1">
+          <h2 className="font-display text-xl text-background">EMAIL</h2>
+          <Separator className="h-0.5 bg-background" />
+          <p className="text-sm">lizmartin@mail.com</p>
+        </div>
+        <div className="flex w-full flex-col items-start justify-start gap-1">
+          <h2 className="font-display text-xl text-background">CITY</h2>
+          <Separator className="h-0.5 bg-background" />
+          <p className="text-sm">Naples</p>
+        </div>
+      </div>
+      <div className="flex w-full items-center justify-start">
+        <Button variant="outline">Logout</Button>
+      </div>
+    </div>
   )
 }
