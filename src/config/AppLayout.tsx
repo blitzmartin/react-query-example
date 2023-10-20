8e7150c96d07f3bbfb2be6ccf1ca8cd0b7024303
blitzmartin <lizmartinmusic@gmail.com>
2023-10-20 19:30:31 +0200

    small changes

--- src/config/AppLayout.tsx
+++ src/config/AppLayout.tsx
@@ -7,7 +7,6 @@ import {
   Toaster
 } from '@/shared'
 import { QueryClientProvider } from '@tanstack/react-query'
-
 import { Outlet } from 'react-router'
 import { queryClient } from './react-query'
 
