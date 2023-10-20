cfa983b74e29437d7b8f1d656bdd2305b7f54be7
blitzmartin <lizmartinmusic@gmail.com>
2023-10-20 19:09:41 +0200

    add avatar

--- src/shared/Sidebar.tsx
+++ src/shared/Sidebar.tsx
@@ -1,6 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from '.'
+
 export const Sidebar = () => {
   return (
-    <div className="h-screen min-w-[240px] bg-primary">
+    <div className="flex h-screen min-w-[240px] flex-col items-center justify-start bg-primary py-8">
+      <Avatar className="h-32 w-32">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>MR</AvatarFallback>
+      </Avatar>
       <p>This</p>
       <p>is</p>
       <p>the</p>
